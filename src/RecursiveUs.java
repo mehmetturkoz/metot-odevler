e6c3e4759c615fcc93ea67409fec0a7a36f70416
mehmet turkoz <mturkozz42@gmail.com>
2022-05-19 12:33:48 +0300

    Recursive ile üslü sayı programı yapıldı.

---
+++ src/RecursiveUs.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class RecursiveUs {
+    static int uslu(int taban,int us){
+        int sonuc=1;
+        if(us==0){
+            return 1;
+        }else if(taban==1){
+            return 1;
+        }else {
+            sonuc *= taban;
+        }
+        return sonuc*uslu(taban,us-1) ;
+    }
+    public static void main(String[] args){
+        int a,b;
+        Scanner input=new Scanner(System.in);
+        System.out.print("Taban sayisini giriniz:");
+        a= input.nextInt();
+        System.out.print("Uslu sayisini giriniz:");
+        b= input.nextInt();
+
+        System.out.println("SONUC:"+uslu(a,b));
+
+    }
+}
